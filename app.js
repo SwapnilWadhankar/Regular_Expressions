2d10930e40589172d794c3cc30ea6e06b62b1ece
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2020-02-26 20:58:31 +0530

    All the cases are added

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let re;
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
+re = /Hel{2,}o/i; // Must occur at least {m} times
 
-re = /hello/i;
+// Paretheses () - Grouping
+re = /^([0-9]x){3}$/;
 
-const str = "Hello";
+// Shorthand Character Classes
+re = /\w/; // Word character - alphanumeric or _
+re = /\w+/; // + = one or more
+re = /\W/; // Non-Word character
+re = /\d/; // Match any digit
+re = /\d+/; // Match any digit 0 or more times
+re = /\D/; // Match any Non-digit
+re = /\s/; // Match whitespace char
+re = /\S/; // Match non-whitespace char
+re = /Hell\b/i; // Word boundary
+
+// Assertions
+re = /x(?=y)/; // Match x only if followed by y
+re = /x(?!y)/; // Match x only if NOT followed by y
+
+// String to match
+const str = "dkjekdxydjekdj";
 
 // Log Results
 const result = re.exec(str);
