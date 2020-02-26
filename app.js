d38b41cf45511868bd387ebd2ebeeee369e11e74
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2020-02-26 20:56:07 +0530

    function is added

---
+++ app.js
@@ -0,0 +1,19 @@
+let re;
+
+re = /hello/i;
+
+const str = "Hello";
+
+// Log Results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str) {
+  if (re.test(str)) {
+    console.log(`${str} matches ${re.source}`);
+  } else {
+    console.log(`${str} does NOT match ${re.source}`);
+  }
+}
+
+reTest(re, str);
